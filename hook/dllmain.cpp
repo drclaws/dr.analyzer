b2f951df10767ae6e8af4252dac0d7883bfefc11
Александр Христосенко <dr.claws.sakh@gmail.com>
2019-06-06 10:55:22 +1000

    DllMain fix

--- hook/dllmain.cpp
+++ hook/dllmain.cpp
@@ -4,27 +4,29 @@
 #include "hook.h"
 #include "hook_funcs.h"
 
+#include <detours.h>
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
+	if (DetourIsHelperProcess()) {
+		return TRUE;
+	}
+
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		if (GetOrigAddresses() == TRUE) {
 			return StartGathering();
 		}
 		else {
 			return FALSE;
 		}
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-		break;
-    case DLL_PROCESS_DETACH:
+	}
+	else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
 		return StopGathering();
-    }
+	}
     return TRUE;
 }
 
