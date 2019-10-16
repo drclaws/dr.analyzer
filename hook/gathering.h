d45472476b9c56e10af4448cc4fda26cffa48be8
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-10-16 11:29:38 +1000

    Description for some elements was added

--- hook/gathering.h
+++ hook/gathering.h
@@ -3,8 +3,12 @@
 #include "GatherInfo.hpp"
 
 
+// Function of gathering process.
+// Must run in special thread.
 void GatherThreadFunc();
 
+// Adds info to current buffer
 void AddToBuff(GatherInfo* info);
 
+// Indicates to end the gathering
 void SetDisconnect();
