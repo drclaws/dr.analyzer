65601d440895ff160baf522186a597c30511bd8b
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-10-16 11:31:07 +1000

    Description for some elements was added

--- hook/hook.h
+++ hook/hook.h
@@ -4,13 +4,21 @@
 #include "BuffObject.hpp"
 
 
+// HMODULE of current library
 extern HMODULE libHModule;
 
+// HANDLE of waiter thread that waits semaphore
 extern HANDLE waiterThread;
+
+// HANDLE of waiter semaphore that indicates ending of info gathering
 extern HANDLE waiterSemaphore;
 
+// HANDLE of semaphore for indicates if necessary to free current library
 extern HANDLE freeLibSemaphore;
 
+// Searches and adds to buffer info about currently using files
 void SearchFileHandles(BuffObject * &currBuff);
 
+// Finds the ability to use certain types and functions from Native API
+// by obtaining the build number of current Windows OS 
 void GetFeaturesSupport();
