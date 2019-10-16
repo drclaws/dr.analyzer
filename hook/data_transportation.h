f501fcbf8e3aa9d29cf63cfd11e6276fde78b262
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-10-16 11:28:29 +1000

    Description for some elements was added

--- hook/data_transportation.h
+++ hook/data_transportation.h
@@ -3,8 +3,11 @@
 #include "BuffObject.hpp"
 
 
+// Initializes thread for data transporting process
 const bool DataTransportInit();
 
+// Indicates to stop data transporting and wait for thread exit
 void DataTransportStop();
 
+// Adds buff to queue for sending
 void SendBuff(BuffObject *info);
